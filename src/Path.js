9053ff64640aec6c8ea9d6a217609e3c2ba531d7
nftushar <nftushar5200@gmail.com>
2023-12-13 00:18:29 +0600

    previus

--- src/Path.js
+++ src/Path.js
@@ -1,43 +1,40 @@
-import React, { useEffect, useState } from "react";
-import Style from "./Style";
+import React, { useEffect, useRef } from "react";
+import Style from "./Style"; // Make sure to import the Style component
 
 const Path = ({ attributes }) => {
-    const { svgPath, strokeColor, mainText, fillColor, strOffset, mainSvg } = attributes;
-    const [svgContent, setSvgContent] = useState(null);
+    const { svgPath, strokeColor, cId, mainText, fillColor, strOffset } = attributes;
 
-    
 
+    const svgRef = useRef(null);
+    const divRef = useRef(null);
     useEffect(() => {
-        if (mainSvg) {
-            fetch(mainSvg)
-                .then((response) => response.text())
-                .then((svgContent) => {
-                    setSvgContent(svgContent);
-                })
-                .catch((error) => {
-                    console.error("Error fetching SVG:", error);
-                });
-        }
-    }, [mainSvg]);
+        const svgElement = svgRef.current;
+        // const divElement = divRef.current;
+
+        const bbox = svgElement.getBBox();
+        const newHeight = bbox.height;
+
+        // Update the height of the SVG container dynamically
+        svgElement.setAttribute("height", newHeight);
+
+        // Set the width of the div based on the SVG width
+        // divElement.style.width = `${bbox.width}px`;
+    }, [svgPath]); // Trigger the effect whenever svgPath changes
 
     return (
         <>
-            <Style attributes={attributes} />
-            <div data-text={mainText} data-url={mainSvg} data-link-url="">
-                {svgContent && (
-                    <svg xmlns="http://www.w3.org/2000/svg" width="250" height="84.4988" viewBox="0 0 250 84.4988">
-                        {/* Include the dynamically loaded SVG content here */}
-                        <g dangerouslySetInnerHTML={{ __html: svgContent }} />
-                        {/* Render the path with stroke and fill */}
-                        <path d={svgPath} id="e-path-f73d05e" stroke={strokeColor} fill={fillColor}></path>
-                        {/* Render the text */}
-                        <text x="50%" y="50%">
-                            <textPath id="e-text-path-f73d05e" href="#e-path-f73d05e" startOffset={`${strOffset}%`}>
+            <Style attributes={attributes} clientId={cId} />
+            <div ref={divRef} id={`bBlocksPath-${cId}`} className="bBlocksPath">
+                <div className="b-text-path">
+                    <svg ref={svgRef} xmlns="http://www.w3.org/2000/svg">
+                        <path id="MyPath" fill={fillColor} stroke={strokeColor} d={svgPath} />
+                        <text className="text" dy="-10" textAnchor="middle" dominantBaseline="middle">
+                            <textPath href="#MyPath" startOffset={strOffset}>
                                 {mainText}
                             </textPath>
                         </text>
                     </svg>
-                )}
+                </div>
             </div>
         </>
     );
